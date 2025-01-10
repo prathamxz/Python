00da3f03ed0741433c7b45084d405d9399c32095
Prathamesh Zagade <prathamzagade1811@gmail.com>
2025-01-10 23:18:42 +0530

    Create functions arguments.py

---
+++ functions arguments.py
@@ -0,0 +1,29 @@
+def average(a,b):
+    print("The average is ", (a+b)/2)
+
+average(10,20)
+
+
+
+# default arguments
+# def name (fname, mname="patty", lname= "watson"):
+#     print("My name is ", fname, mname, lname)
+
+# name("john")
+
+    #   output - My name is  john patty watson
+
+
+
+
+
+# Function Arguments and return statement : 
+
+# There are four types of arguments that we can provide in a function:
+
+# • Default Arguments
+# • Keyword Arguments
+# • Variable length Arguments
+# • Required Arguments
+# Default arguments:
+# We can provide a default value while creating a function. This way the function assumes a default value even if a value is not provided in the function call for that argument.
