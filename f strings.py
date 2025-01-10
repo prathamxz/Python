5aa22381600b1028602d3d3dfaabc7c8a571356b
Prathamesh Zagade <prathamzagade1811@gmail.com>
2025-01-11 01:31:34 +0530

    Update f strings.py

--- f strings.py
+++ f strings.py
@@ -2,3 +2,8 @@ letter = "Hey my name is {} and i am from {}"
 country = "India"
 name = "Pratham"
 print(letter.format(name,country))
+print(f"""Hey my name is {name} and i am from {country}""")
+
+price = 49.66
+txt = f"for only {price:.2f} dollars!"
+print(txt)
