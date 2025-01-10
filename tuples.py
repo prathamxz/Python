40f13b0317219823543cf9edd8f7c3333d8773c5
Prathamesh Zagade <prathamzagade1811@gmail.com>
2025-01-11 01:08:10 +0530

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -1,2 +1,11 @@
-tup = (1,4,6,44,6,3,1,3,5)
+tup = (1,4,6,44,6,3,1,3,5,"turs",True)
 print(type(tup),tup)
+print(tup[0])
+print(tup[3])
+print(tup[4])
+print(len(tup))
+
+if 44 in tup:
+    print("Yes present in tuple")    
+else:
+    print("No")
