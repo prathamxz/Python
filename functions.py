003d7ae7ed97d676b66a8a6a643d8ae21ceae491
Prathamesh Zagade <prathamzagade1811@gmail.com>
2025-01-10 21:56:05 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -38,3 +38,29 @@ isGreater(c,d)
 # gmean2=(c*d)/(c+d)
 # print(gmean2)
 calculateGmean(c,d)
+
+
+# Functions:
+# A function is a block of code that performs a specific task whenever it is called. In bigger programs, where we have large amounts of code, it is advisable to create or use existing functions that make the program flow organized and neat.
+# There are two types of functions:
+# 1. Built-in functions
+# 2. User-defined functions
+
+# Built-in functions:
+# These functions are defined and pre-coded in python. Some examples of built-in functions are as follows:
+# minO, max0, lenO, sum0, type, range, dict0, listO, tuple, setO, printO, etc.
+
+# # User-defined functions:
+# User-defined functions:
+# We can create functions to perform specific tasks as per our needs.
+# Such functions are called user-defined functions.
+
+# Syntax:
+# def function_name(parameters) :
+# pass
+
+
+# • Create a function using the def keyword, followed by a function name, followed by a paranthesis (0) and a colon(;).
+# • Any parameters and arguments should be placed within the parentheses.
+# • Rules to naming function are similar to that of naming variables.
+# • Any statements and other code within the function should be indented.
