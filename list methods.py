d439561113790b845b154502c3c5de7c2ec65351
Prathamesh Zagade <prathamzagade1811@gmail.com>
2025-01-11 01:01:24 +0530

    Update list methods.py

--- list methods.py
+++ list methods.py
@@ -1,7 +1,26 @@
 l = [11,22,3,33,1,2,4,6]
-print(l)
-l.append(7)
-print(l)
+# print(l)
+# l.append(7)
+# print(l)
 
-l.sort()
-print(l)
+# l.sort(reverse=True)
+# print(l)
+
+# print(l.index(33))
+
+# print(l.count(33))
+
+# m=l
+# m[0]=0
+
+# l.insert(1,980)
+# print(l)
+
+# m=[900,394,1100]
+# k=l+m
+# l.extend(m)
+# print(l)
+# print(k)
+
+
+
