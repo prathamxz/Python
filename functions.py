2958b7e15a3ef23cbaaa7b77d1702a53e322a9da
Prathamesh Zagade <prathamzagade1811@gmail.com>
2025-01-10 21:44:21 +0530

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,26 @@
+def calculateGmean (a ,b):
+    mean=(a*b)/(a+b)
+    print(mean)
+
+
+
+a = 5
+b=6
+
+if(a>b):
+    print("First number is greater than second number")
+else:
+    print("Second number is greater than first number")
+calculateGmean(a,b)
+# gmean1=(a*b)/(a+b)
+# print(gmean1)
+
+c=2
+d=1
+if(c>d):
+    print("First number is greater than second number")
+else:
+    print("Second number is greater than first number")
+# gmean2=(c*d)/(c+d)
+# print(gmean2)
+calculateGmean(c,d)
