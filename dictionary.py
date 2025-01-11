c1dd957c80e3a1ed5eb11c3b9baa4aabd5d165f2
Prathamesh Zagade <prathamzagade1811@gmail.com>
2025-01-11 23:48:46 +0530

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -5,10 +5,17 @@
 # print(dict["harry"])
 # print(dict["Spoon"])
 
-dic = {
-    44 : "Hari",
-    66 : "Neha",
-    11 : "Nora",
-    1 : "Raj"
-}
-print(dic[1])
+# dic = {
+#     44 : "Hari",
+#     66 : "Neha",
+#     11 : "Nora",
+#     1 : "Raj"
+# }
+# print(dic[1])
+
+info = {
+    'Name' : "Pratham", "Age" : 18 ,"Eligibile" : "True"
+} 
+print(info)
+print(info['Name'])
+print(info.get('Name'))
