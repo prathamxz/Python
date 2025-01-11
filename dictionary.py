1860771e91f318151021329d36b3aff1ebb21973
Prathamesh Zagade <prathamzagade1811@gmail.com>
2025-01-11 21:13:56 +0530

    Update dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,14 @@
+# dict = {
+#     "harry" : "Human being",
+#     "Spoon" : "object"
+# }
+# print(dict["harry"])
+# print(dict["Spoon"])
+
+dic = {
+    44 : "Hari",
+    66 : "Neha",
+    11 : "Nora",
+    1 : "Raj"
+}
+print(dic[1])
