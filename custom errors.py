5711a03d7a1782ba332ca8644fc46d3cb45f4b6e
Prathamesh Zagade <prathamzagade1811@gmail.com>
2025-01-20 22:00:38 +0530

    Update custom errors.py

--- custom errors.py
+++ custom errors.py
@@ -2,5 +2,8 @@ a = int(input("Enter any value between 5 and 9 : "))
 
 if (a <5 or a > 9):
     raise ValueError("The value should be between 5 and 9")
-else:
-    print("The value entered is : ", a )
+# else:
+#     print(("The value entered is :") ,a, ("and it is in between 5 and 9"))
+
+    # a = 7  # Assuming 'a' is defined somewhere in your code
+    # print("The value entered is:", a, "and it is in between 5 and 9")
