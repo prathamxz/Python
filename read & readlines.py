328da1fbf8d3deb5594e554aff545043897788cd
Prathamesh Zagade <prathamzagade1811@gmail.com>
2025-01-21 23:08:17 +0530

    co

---
+++ read & readlines.py
@@ -0,0 +1,17 @@
+# f= open ('myfile.txt','r')
+# i=0
+# while True: 
+#     line= f.readline()
+#     if not line:
+#         print(line, type(line))
+#         break 
+#     m1 = int(line.split(",")[0])
+#     m2 = int(line.split(",")[1])
+#     m3 = int(line.split(",")[2])
+#     print(f"Marks of student {i} in maths is {m1}")
+#     print(f"Marks of student {i} in sst is {m2}")
+#     print(f"Marks of student {i} in physics is {m3}")
+
+   
+
+
