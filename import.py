96f003cf181422836573f095fbd462e32f22c81f
Prathamesh Zagade <prathamzagade1811@gmail.com>
2025-01-21 15:14:32 +0530

    commit

---
+++ import.py
@@ -0,0 +1,36 @@
+# import pandas 
+# pandas.read_csv()
+
+# import math
+# a = math.floor(22.23839)
+# print(a)
+
+
+# import math
+# from math import sqrt, pi 
+# result= sqrt(225)
+# result2= sqrt(225) * pi
+# b = math.floor(result)
+# print(b)
+# print(result2)
+# print(pi)
+
+
+# import math as m
+# from math import sqrt as s 
+# resu = s(225)
+# print(resu)
+
+# import math
+# print(dir(math))  #list of all functions in math module
+# print(math.nan, type(math.nan))
+
+# print("\n")
+
+# import pandas
+# print(dir(pandas))  #list of all functions in pandas module
+
+
+from pratham import gg
+import pratham
+print(gg)
---
+++ pratham.py
@@ -0,0 +1,4 @@
+def pratham():
+    print("hey welcome to the club")
+
+gg = "you are a good boy"
