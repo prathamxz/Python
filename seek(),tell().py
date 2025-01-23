1f3ff37888224f449220e81d68fbabe064fbc37d
Prathamesh Zagade <prathamzagade1811@gmail.com>
2025-01-23 11:33:34 +0530

    summary

--- seek(),tell().py
+++ seek(),tell().py
@@ -1,4 +1,16 @@
+# The seek function allows you to move the current position within a file to a specific point. The position is specified in bytes, and you can move either forward or backward from the current position. For example:
 
+with open( 'file.txt', 'r') as f:
+    print(type(f))
+# Move to the 10th byte in the file
+    f.seek(10)
+# Read the next 5 bytes
+    data = f. read (5)
+    print(data)
+
+# tellO function
+# The tellO function returns the current position within the file, in bytes.
+# This can be useful for keeping track of your location within the file or for seeking to a specific position relative to the current position.
 
 
 
