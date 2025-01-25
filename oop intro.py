a88b1ee30b96eb076d47dde8e318bba127e38c6f
Prathamesh Zagade <prathamzagade1811@gmail.com>
2025-01-26 00:48:59 +0530

    Create oop intro.py

---
+++ oop intro.py
@@ -0,0 +1,38 @@
+def hello():
+    print("Hello")
+
+hello()
+sales1 = 6000
+profit1 = 2000
+ad1 = 1000
+
+sales2 = 6000
+profit2 = 2000
+ad2 = 1000
+
+sales3 = 6000
+profit3 = 2000
+ad3 = 1000
+
+
+
+
+
+
+
+
+
+
+
+# Introduction to Object-oriented programming
+
+# Introduction to Object-Oriented Programming in Python: In programming languages, mainly there are two approaches that are used to write program or code.
+
+# • 1). Procedural Programming A
+# • 2). Object-Oriented Programming
+
+# The procedure we are following till now is the "Procedural Programming" approach. So, in this session, we will learn about Object Oriented Programming (OOP). The basic idea of object-oriented programming (OOP) in Python is to use classes and objects to represent real-world concepts and entities.
+# A class is a blueprint or template for creating objects. It defines the properties and methods that an object of that class will have. Properties are the data or state of an object, and methods are the actions or behaviors that an object can perform.
+# An object is an instance of a class, and it contains its own data and methods. For example, you could create a class called "Person" that has properties such as name and age, and methods such as speak and walkO. Each instance of the Person class would be a unique object with its own name and age, but they would all have the same methods to speak and walk.
+# One of the key features of OOP in Python is encapsulation, which means that the internal state of an object is hidden and can only be accessed or modified through the object's methods. This helps to protect the object's data and prevent it from being modified in unexpected ways.
+# Another key feature of OOP in Python is inheritance, which allows new classes to be created that inherit the properties and methods of an existing class. This allows for code reuse and makes it easy to create new classes that have similar functionality to existing classes.
